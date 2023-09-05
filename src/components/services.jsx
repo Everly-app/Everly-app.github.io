980d8c0522ce865c7f5191c9d33fb0e8771a9740
Jared Junyoung Lim <jylim9823@gmail.com>
2023-09-04 20:10:45 -0700

    Fix service alignment

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -2,12 +2,12 @@ export const Services = (props) => {
   return (
     <div id='services' className='text-center'>
       <div className='container'>
-        <div className='row'>
+        <div className='row-fluid'>
           {props.data
             ? props.data.map((d, i) => (
                 <div key={`${d.name}-${i}`} className='col-md-4'>
                   {' '}
-                  <div className='service-img'>
+                  <div>
                     <img src={d.img} alt='' />{' '}
                   </div>
                   <div className='service-desc'>
