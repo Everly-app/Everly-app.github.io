c67aaf7954340ab5e8de585fddeeb4c37f67e3bc
Jared Junyoung Lim <jylim9823@gmail.com>
2023-09-05 01:49:34 -0700

    Add hash router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 // import { Navigation } from './components/navigation'
 import { Header } from './components/header'
 import { SignUp } from './components/signup'
