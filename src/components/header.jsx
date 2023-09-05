e11176fecce2f962e023dc3ec30d15ffae5e74e8
Jared Junyoung Lim <jylim9823@gmail.com>
2023-09-04 23:51:04 -0700

    particle animation

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,38 @@
-// import ParticlesBg from "particles-bg";
+import ParticlesBg from "particles-bg";
 
 export const Header = (props) => {
+  let config = {
+    num: [20, 30],
+    rps: 10,
+    radius: [20, 30],
+    life: [1, 15],
+    theta: [-4000, -4000],
+    v: [0, 0],
+    alpha: [100, 100],
+    scale: [1, 1],
+    position: "all", // all or center or {x:1,y:1,width:100,height:100}
+    color: "radial-gradient(#ffffff00, #ffffffff)",
+    cross: "dead", // cross or bround
+    random: null,  // or null,
+    g: 0,    // gravity
+    // f: [-0.1, 0.1], // force
+    onParticleUpdate: (ctx, particle) => {
+        ctx.beginPath();
+        ctx.arc(particle.p.x, particle.p.y, particle.radius * 2, Math.PI * 2, false);
+        const my_gradient = ctx.createRadialGradient(
+          particle.p.x, particle.p.y, particle.radius * 0.5,
+          particle.p.x, particle.p.y, particle.radius);
+        my_gradient.addColorStop(0, "#ffff0008");
+        my_gradient.addColorStop(1, "#ffff0005");
+        ctx.fillStyle = my_gradient;
+        ctx.fill();
+        ctx.closePath();
+    }
+  };
   return (
     <header id='header'>
       <div className='intro'>
-        {/* <ParticlesBg type="circle" color="#ffffffff" bg={{zIndex: 0, position:"absolute", top:0}} /> */}
+        <ParticlesBg type="custom" config={config} bg={{zIndex: 0, position:"absolute", top:0}} />
         <div className='overlay'>
           <div className='container'>
           
